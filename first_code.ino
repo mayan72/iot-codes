f16a183c2af6b787d9bbce5cbdebfa6a58bc311c
Mayank Gupta <68817462+mayan72@users.noreply.github.com>
2022-01-10 11:05:05 +0530

    Add files via upload

---
+++ first_code.ino
@@ -0,0 +1,44 @@
+//void setup() {
+//  // put your setup code here, to run once:
+// pinMode(LED_BUILTIN, OUTPUT);
+//}
+//
+//void loop() {
+//  // put your main code here, to run repeatedly:
+//digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+//  delay(10);                       // wait for a second
+//  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off by making the voltage LOW
+//  delay(1000);    
+//}
+
+//
+//int LED = 13;
+//void setup(){
+//  pinMode(LED , OUTPUT);
+//  
+//}
+//
+//void loop(){
+//  digitalWrite(LED, HIGH);
+//  delay(1000);
+//  digitalWrite(LED, LOW);
+//  delay(1000);
+//}
+
+//fade
+//
+//int led =13;
+//int bright=100;
+//int fade=50;
+//void setup(){
+//pinMode(led,OUTPUT);
+//  
+//}
+//void loop(){
+//  analogWrite(led, bright);
+//  bright=bright+fade;
+//  if(bright<=0 || bright>=255){
+//    fade=-fade;
+//  }
+//  delay(30);
+//}
---
+++ humidity.ino
@@ -0,0 +1,3 @@
+#include <dht.h>
+
+#include <dht.h>
